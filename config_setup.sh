df9f73f2c0ab02220ee11cf2937124abbe3e5828
joshp123 <joshp123@users.noreply.github.com>
2017-05-10 12:20:33 +0200

    Update config_setup.sh

--- config_setup.sh
+++ config_setup.sh
@@ -2,12 +2,7 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 git clone --recursive https://github.com/Eriner/zim.git ${ZDOTDIR:-${HOME}}/.zim
 
-setopt EXTENDED_GLOB
-for template_file ( ${ZDOTDIR:-${HOME}}/.zim/templates/* ); do
-  user_file="${ZDOTDIR:-${HOME}}/.${template_file:t}"
-  touch ${user_file}
-  ( print -rn "$(<${template_file})$(<${user_file})" >! ${user_file} ) 2>/dev/null
-done
+git clone git://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 
 chsh -s =zsh
 
